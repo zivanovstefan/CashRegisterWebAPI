0194d1b321da00232ccc112b492ef3a9b4b5dd6b
Stefan Zivanov <zivanovstefan@gmail.com>
2022-03-07 20:41:56 +0100

    Models and DbContext added. Database generated with EntityFramework.

---
+++ CashRegister.Domain/Models/Bill.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace CashRegister.Domain.Models
+{
+    public class Bill
+    {
+        [Key]
+        public int Id { get; set; }
+        public string PaymentMethod { get; set; }
+        public decimal TotalPrice { get; set; }
+        IQueryable<Product> Products { get; set; }
+    }
+}
---
+++ CashRegister.Infrastructure/Context/CashRegisterDBContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CashRegister.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CashRegister.Infrastructure.Context
+{
+    public class CashRegisterDBContext : DbContext
+    {
+        public CashRegisterDBContext(DbContextOptions<CashRegisterDBContext> options) : base(options)
+        { 
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Bill> Bills { get; set; }
+    }
+}
