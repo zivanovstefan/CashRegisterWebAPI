b552c1409c1b5c7f7cd793355dda29ea4f9c3fe2
Stefan Zivanov <zivanovstefan@gmail.com>
2022-03-07 16:37:32 +0100

    Base classes and connection string added

---
+++ CashRegister.Application/AutoMapper/AutoMapperConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+
+namespace CashRegister.Application.AutoMapper
+{
+    public class AutoMapperConfiguration
+    {
+    }
+}
